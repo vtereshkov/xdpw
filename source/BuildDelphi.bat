30f71a3ba7465dfb97ab1b4f2587bbfa43b0a744
Vasiliy Tereshkov <52492460+vtereshkov@users.noreply.github.com>
2019-08-16 03:54:26 +0300

    Revert "Open array parameters added"
    This reverts commit e9aa31438e646f1fb6de7b2f387554c05a820ebe.

--- source/BuildDelphi.bat
+++
@@ -1,8 +0,0 @@
-cd source
-copy XDPWCustomDelphi.inc XDPWCustom.inc /y
-dcc32 xdpw.pas
-copy xdpw.exe ..\xdpw.exe /y
-del xdpw.exe, XDPWCustom.inc
-cd ..
-
-
--- source/BuildFPC.bat
+++
@@ -1,6 +0,0 @@
-cd source
-copy XDPWCustomDelphi.inc XDPWCustom.inc /y
-fpc -Sd xdpw.pas
-copy xdpw.exe ..\xdpw.exe /y
-del xdpw.exe, XDPWCustom.inc, *.o
-cd ..
