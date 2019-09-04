ed8b60ea53e2c1e8b602da13795fcf43b71b9bce
Vasiliy Tereshkov <52492460+vtereshkov@users.noreply.github.com>
2019-09-04 03:09:27 +0300

    Update BuildSelf.bat

--- BuildSelf.bat
+++ BuildSelf.bat
@@ -1,8 +1,8 @@
 cd source
 copy ..\xdpw.exe xdpwold.exe /y
 copy ..\system.inc system.inc /y
-copy XDPWCustomXDPW.inc XDPWCustom.inc /y
+copy CustomXDPW.inc Custom.inc /y
 xdpwold xdpw.pas
 copy xdpw.exe ..\xdpw.exe /y
-del xdpw.exe, xdpwold.exe, system.inc, XDPWCustom.inc
+del xdpw.exe, xdpwold.exe, system.inc, Custom.inc
 cd ..
